83005205b66b9eea4bde81c955c3eed1afac2a1f
nathaliaamoyses-hash <nathaliaa.moyses@gmail.com>
2026-09-08 20:08:05 -0300

    Create config.js

---
+++ src/data/clients/crefaz/config.js
@@ -0,0 +1,12 @@
+export const client = {
+  id: 'crefaz',
+  name: 'Crefaz',
+  agendaTitle: 'Crefaz @ Dreamforce 2026',
+  logo: '/icons/acerto-header-v1.png',
+  appIcons: { small: '/icons/acerto-df192-v1.png', large: '/icons/acerto-df512-v1.png' },
+  heroImage: null,
+  whatsappNumber: '5511930227247',
+  teamContacts: [{ name: 'Fabiana', phone: '5511930227247' }],
+  favoritesKey: 'df26-crefaz-favorites',
+  sundayPreferenceKey: 'df26-crefaz-sunday-preference',
+};
